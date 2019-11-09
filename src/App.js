1da65570df8857f43edd1e6e6e7c59acb761d815
frknckc <furkancakici25@gmail.com>
2019-11-09 16:28:08 +0300

    Teslim 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Hamburger, TotalPrice, IngredientList } from './components';
+import { Hamburger, TotalPrice, IngredientListgi } from './components';
 
 function App() {
   return (
